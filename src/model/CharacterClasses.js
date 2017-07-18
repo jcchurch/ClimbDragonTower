10fb83661f62227c376dd636f3b39b46be23b959
James Church <jcchurch@gmail.com>
2017-07-17 22:08:57 -0500

    Adding files. Do not use.

---
+++ src/model/CharacterClasses.js
@@ -0,0 +1,138 @@
+/**
+
+This file is part of Climb Dragon Mountain.
+
+Hunt the Yeti is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+module.exports = {
+    "Fighter": {
+        "hitpoints": 30,
+        "nearattack": 6,
+        "farattack": 3,
+        "defense": 5,
+        "elemental_advantage": cold,
+        "elemental_weakness": fire,
+        "feats": ["flurry"]
+    }
+    ,"Wizard": {
+        "hitpoints": 30,
+        "nearattack": 5,
+        "farattack": 6,
+        "defense": 3,
+        "elemental_advantage": acid,
+        "elemental_weakness": cold,
+        "feats": ["lightning"]
+    }
+    ,"Rogue": {
+        "hitpoints": 30,
+        "nearattack": 2,
+        "farattack": 6,
+        "defense": 6,
+        "elemental_advantage": fire,
+        "elemental_weakness": acid,
+        "feats": ["sneak_attack"]
+    }
+    ,"Skeleton": {
+        "hitpoints": 2,
+        "nearattack": 2,
+        "farattack": 1,
+        "defense": 3,
+        "elemental_advantage": cold,
+        "elemental_weakness": acid,
+        "feats": ["first_strike"]
+    },
+    ,"Basilisk": {
+        "hitpoints": 12,
+        "nearattack": 6,
+        "farattack": 4,
+        "defense": 5,
+        "elemental_advantage": acid,
+        "elemental_weakness": cold,
+        "feats": ["stun"]
+    },
+    ,"Orc": {
+        "hitpoints": 18,
+        "nearattack": 8,
+        "farattack": 4,
+        "defense": 8,
+        "elemental_advantage": fire,
+        "elemental_weakness": fire,
+        "feats": []
+    },
+    ,"Troll": {
+        "hitpoints": 14,
+        "nearattack": 6,
+        "farattack": 3,
+        "defense": 7,
+        "elemental_advantage": cold,
+        "elemental_weakness": fire,
+        "feats": ["regenerate"]
+    },
+    ,"Vampire": {
+        "hitpoints": 16,
+        "nearattack": 2,
+        "farattack": 6,
+        "defense": 6,
+        "elemental_advantage": fire,
+        "elemental_weakness": acid,
+        "feats": ["drain_life"]
+    },
+    ,"Spider": {
+        "hitpoints": 10,
+        "nearattack": 2,
+        "farattack": 6,
+        "defense": 4,
+        "elemental_advantage": cold,
+        "elemental_weakness": cold,
+        "feats": ["poison"]
+    },
+    ,"Wisp": {
+        "hitpoints": 4,
+        "nearattack": 2,
+        "farattack": 6,
+        "defense": 2,
+        "elemental_advantage": acid,
+        "elemental_weakness": acid,
+        "feats": ["dodge"]
+    },
+    ,"Goblin": {
+        "hitpoints": 8,
+        "nearattack": 4,
+        "farattack": 0,
+        "defense": 4,
+        "elemental_advantage": fire,
+        "elemental_weakness": cold,
+        "feats": ["flurry"]
+    },
+    ,"Giant Bat": {
+        "hitpoints": 6,
+        "nearattack": 3,
+        "farattack": 5,
+        "defense": 3,
+        "elemental_advantage": acid,
+        "elemental_weakness": fire,
+        "feats": ["acid_spit"]
+    },
+    ,"Dragon": {
+        "hitpoints": 40,
+        "nearattack": 30,
+        "farattack": 30,
+        "defense": 15,
+        "elemental_advantage": rolled,
+        "elemental_weakness": rolled,
+        "feats": ["ground_pound"]
+    }
+};
